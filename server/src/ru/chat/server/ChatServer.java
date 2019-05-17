b7680e4095ab2c32da31f9648e812d40af749067
Разгуляева Анастасия Дмитриевна OUT <Razgulyaeva.An.Dm@sberbank.ru>
2019-05-17 09:49:32 +0300

    Чат с Geek Brains: три модуля, swing окна

---
+++ server/src/ru/chat/server/ChatServer.java
@@ -0,0 +1,63 @@
+package ru.chat.server;
+
+import ru.chat.network.TCPConnection;
+import ru.chat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer implements TCPConnectionListener {
+
+    private static final int SERVER_PORT = 8189;
+    private final List<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        for (TCPConnection connection : connections) {
+            connection.sendString(value);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+}
